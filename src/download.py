45f562adb1d49ae60866caabf9ea3459ae1d15f8
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-02-27 18:08:47 -0800

    leaving data local for now

--- src/download.py
+++
@@ -1,8 +0,0 @@
-import requests
-import gzip
-
-url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00279/SUSY.csv.gz'
-filename = 'data/raw/SUSY.csv'
-with open(filename, 'wb') as ofile:
-   response = gzip.open(requests.get(url))
-   ofile.write(response.content)
