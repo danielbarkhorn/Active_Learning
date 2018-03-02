28603a2970769feefb5472b9826283a48bec23ec
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-02 12:48:30 -0800

    consolidatig data and sampling. Fixing imports finally

--- src/test.py
+++ src/test.py
@@ -1,7 +1,9 @@
-from sampling import sampler as s
+from data import dataset as d
 import numpy as np
 
 test_data = np.random.randn(100, 5) + 1
 test_sample = s.Sampler(test_data)
+
+susy_data = d.Dataset()
 
 print(test_sample.systematic(sort='feature'))
