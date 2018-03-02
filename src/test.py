3daedf6e3bac0e0fb2f5058557e1ba5aa856c1c2
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-01 17:45:19 -0800

    finalizing structure. Explaining imports

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
-from sampling import Sampling
+from sampling import sampler as s
 import numpy as np
 
 test_data = np.random.randn(100, 5) + 1
-test_sample = Sampling(test_data)
+test_sample = s.Sampler(test_data)
 
 print(test_sample.systematic(sort='feature'))
