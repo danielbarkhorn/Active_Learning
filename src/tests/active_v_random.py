fc1d157324e1efc11ee8384f5be1721a1c0573b5
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-05 22:02:53 -0800

    added a test function for model, also finalized the test file

--- src/tests/active_v_random.py
+++ src/tests/active_v_random.py
@@ -12,24 +12,19 @@ from models.activelearn import Active_Learner
 data = Dataset('SUSY_100k.csv').random_sample(.01) #1k points
 (total_train, total_test) = data.test_train_split(train_percent=.8)
 
-rand_SVM700 = Model('SVM')
-rand_SVM700.fit(total_train.get_x(), total_train.get_y())
+rand_SVM800 = Model('SVM')
+rand_SVM800.fit(total_train.get_x(), total_train.get_y())
 
 train160 = total_train.random_sample(.2)
 rand_SVM160 = Model('SVM')
 rand_SVM160.fit(train160.get_x(), train160.get_y())
 
-active_SVM = Model('SVM')
+active_SVM = Model('SVM', active=True)
 AL_SVM = Active_Learner(model=active_SVM, start_size=.1, end_size=.2, step_size=.005)
 active_SVM = AL_SVM.fit(total_train.get_x(), total_train.get_y())
 
-from sklearn.metrics import classification_report
+rand_SVM800.test(total_test.get_x(), total_test.get_y())
 
-print('Random SVM, 700 pts')
-print (classification_report(total_test.get_y(),rand_SVM700.predict(total_test.get_x(), proba=False)))
+rand_SVM160.test(total_test.get_x(), total_test.get_y())
 
-print('Random SVM, 160 pts')
-print (classification_report(total_test.get_y(),rand_SVM160.predict(total_test.get_x(), proba=False)))
-
-print('Active Learned SVM, 160 pts')
-print (classification_report(total_test.get_y(),active_SVM.predict(total_test.get_x(), proba=False)))
+active_SVM.test(total_test.get_x(), total_test.get_y())
