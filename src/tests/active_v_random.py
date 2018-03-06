4b360c6eaded684542b1280a909f5d043e37f4f4
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-05 22:21:18 -0800

    adding test results handler

--- src/tests/active_v_random.py
+++ src/tests/active_v_random.py
@@ -8,23 +8,32 @@ from data.dataset import Dataset
 from models.model import Model
 from models.activelearn import Active_Learner
 
-
+# Make our data
 data = Dataset('SUSY_100k.csv').random_sample(.01) #1k points
 (total_train, total_test) = data.test_train_split(train_percent=.8)
+train160 = total_train.random_sample(.2)
 
+sys_train160 = total_train.systematic_sample(percent=0.2)
+
+# Make our models
 rand_SVM800 = Model('SVM')
 rand_SVM800.fit(total_train.get_x(), total_train.get_y())
 
-train160 = total_train.random_sample(.2)
 rand_SVM160 = Model('SVM')
 rand_SVM160.fit(train160.get_x(), train160.get_y())
 
-active_SVM = Model('SVM', active=True)
+sys_SVM160 = Model('SVM', sample='Systematic')
+sys_SVM160.fit(sys_train160.get_x(), sys_train160.get_y())
+
+active_SVM = Model('SVM', sample='Active')
 AL_SVM = Active_Learner(model=active_SVM, start_size=.1, end_size=.2, step_size=.005)
 active_SVM = AL_SVM.fit(total_train.get_x(), total_train.get_y())
 
-rand_SVM800.test(total_test.get_x(), total_test.get_y())
+# Test our models
+rand_SVM800.test(total_test.get_x(), total_test.get_y(), fname='results.txt')
 
-rand_SVM160.test(total_test.get_x(), total_test.get_y())
+rand_SVM160.test(total_test.get_x(), total_test.get_y(), fname='results.txt')
 
-active_SVM.test(total_test.get_x(), total_test.get_y())
+sys_SVM160.test(total_test.get_x(), total_test.get_y(), fname='results.txt')
+
+active_SVM.test(total_test.get_x(), total_test.get_y(), fname='results.txt')
