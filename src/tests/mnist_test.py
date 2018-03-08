d9e4794d67eec6e482edcbc6fb3534177de3592e
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-07 19:30:24 -0800

    fixed PCA

--- src/tests/mnist_test.py
+++ src/tests/mnist_test.py
@@ -11,8 +11,13 @@ from models.activelearn import Active_Learner
 mnist_data = Dataset(filename='MNIST.csv', header=1).random_sample(percent=.1)
 mnist_pca = mnist_data.pca(n_components=50)
 (train, test) = mnist_pca.test_train_split(train_percent=.8)
+rand_data = train.random_sample(percent=.2)
 
-print(mnist_pca[:50])
-# rand_SVM = Model('SVM')
-# rand_SVM.fit(train.get_x(), train.get_y())
-# rand_SVM.test(test.get_x(), test.get_y(), fname='results.txt')
+rand_SVM = Model('KNN')
+rand_SVM.fit(rand_data.get_x(), rand_data.get_y())
+rand_SVM.test(test.get_x(), test.get_y(), fname='results.txt')
+
+# active_SVM = Model('SVM', sample='Active')
+# AL_SVM = Active_Learner(model=active_SVM, start_size=.1, end_size=.2, step_size=.05)
+# active_SVM = AL_SVM.fit(train.get_x(), train.get_y())
+# active_SVM.test(test.get_x(), test.get_y(), fname='results.txt')
