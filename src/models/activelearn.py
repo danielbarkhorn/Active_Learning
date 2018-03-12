1c3a006b91f940801ca473861756d7d1f799e40d
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-11 21:42:29 -0700

    deleting unneeded file fixing capitalization

--- src/models/activelearn.py
+++ src/models/activelearn.py
@@ -9,24 +9,24 @@ class Active_Learner:
         self.end_size = end_size
         self.step_size = step_size
 
-    def fit(self, X, y):
-        X_train, X_unlabeled, y_train, y_unlabeled = train_test_split(X, y, test_size=1-self.start_size, random_state=42)
+    def fit(self, X, Y):
+        X_train, X_unlabeled, Y_train, Y_unlabeled = train_test_split(X, Y, test_size=1-self.start_size, random_state=42)
 
-        while(len(y_train) <= len(y)*self.end_size):
-            self.model.fit(X_train,y_train)
-            y_unlabeled_hat = self.model.predict(X_unlabeled)
+        while(len(Y_train) <= len(Y)*self.end_size):
+            self.model.fit(X_train,Y_train)
+            Y_unlabeled_hat = self.model.predict(X_unlabeled)
 
             #select points with low confidence (class probabilty close to 0.5)
-            lowest_conf_idx = np.flip(np.argsort(abs(0.5-y_unlabeled_hat[:,0])),axis=0)
+            lowest_conf_idx = np.flip(np.argsort(abs(0.5-Y_unlabeled_hat[:,0])),axis=0)
 
             #add points of least confidence to training set
-            X_train = np.concatenate((X_train,X_unlabeled[:int(len(y)*self.step_size)]),axis=0)
-            y_train = np.concatenate((y_train,y_unlabeled[:int(len(y)*self.step_size)]),axis=0)
+            X_train = np.concatenate((X_train,X_unlabeled[:int(len(Y)*self.step_size)]),axis=0)
+            Y_train = np.concatenate((Y_train,Y_unlabeled[:int(len(Y)*self.step_size)]),axis=0)
 
             #remove these points from "unlabeled" set
-            mask = np.ones(len(y_unlabeled), dtype=bool)
-            mask[lowest_conf_idx[0:int(len(y)*self.step_size)]] = False
-            y_unlabeled = y_unlabeled[mask]
+            mask = np.ones(len(Y_unlabeled), dtype=bool)
+            mask[lowest_conf_idx[0:int(len(Y)*self.step_size)]] = False
+            Y_unlabeled = Y_unlabeled[mask]
             X_unlabeled = X_unlabeled[mask]
 
         return self.model
--- src/notebooks/temp.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-src_dir = os.path.join(os.getcwd(), os.pardir)
-sys.path.append(src_dir)
-
-from data.dataset import Dataset
-
-Dataset('SUSY_100k.csv')
