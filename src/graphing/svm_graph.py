ae04b7e6c5fe234d28c58734142adcd2a7bcf7cd
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-14 21:23:00 -0700

    adding and commenting systematic on graph

--- src/graphing/svm_graph.py
+++ src/graphing/svm_graph.py
@@ -13,6 +13,7 @@ svmRandomF1 = {}
 svmSysF1 = {}
 act_means = []
 rand_means = []
+sys_means = []
 
 
 # Graphing
@@ -29,12 +30,15 @@ for sampleSize in range(50, 301, 10):
     svmSysF1[sampleSize] = pickle.load(open(sSVMfname, "rb"))
 
     X = [sampleSize] * 50
-    plt.scatter([x - 0.5 for x in X], svmRandomF1[sampleSize][:50], s=5, c='Blue', alpha=0.05)
-    plt.scatter([x + 0.5 for x in X], svmActiveF1[sampleSize][10][:50], s=5, c='Red', alpha=0.075)
+    plt.scatter(X, svmRandomF1[sampleSize][:50], s=8, c='Blue', alpha=0.075)
+    plt.scatter(X, svmActiveF1[sampleSize][10][:50], s=8, c='Red', alpha=0.075)
+    #plt.scatter(X, svmSysF1[sampleSize][:50], s=8, c='Green', alpha=0.075)
 
     rand_means.append(np.mean(svmRandomF1[sampleSize]))
     act_means.append(np.mean(svmActiveF1[sampleSize][10]))
+    sys_means.append(np.mean(svmSysF1[sampleSize][10]))
 
 plt.plot(range(50, 301, 10), rand_means,linewidth=3.0,alpha=0.5)
 plt.plot(range(50, 301, 10), act_means,linewidth=3.0, c='Red',alpha=0.5)
+#plt.plot(range(50, 301, 10), sys_means,linewidth=3.0, c='Green',alpha=0.5)
 plt.show()
