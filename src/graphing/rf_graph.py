beb225140b904580dcdf0d7566c9be284044bb80
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-04-09 12:53:41 -0700

    adding graph of active learning on hyperplane distances

--- src/graphing/rf_graph.py
+++ src/graphing/rf_graph.py
@@ -18,7 +18,7 @@ sys_means = []
 
 
 # Graphing
-plt.xlabel('Sample Size')
+plt.xlabel('Training Size')
 plt.ylabel('F1 Score')
 
 for sampleSize in range(50, 211, 10):
@@ -33,17 +33,21 @@ for sampleSize in range(50, 211, 10):
     X = [sampleSize] * 150
     plt.scatter(X, rfRandomF1[sampleSize], s=8, c='Blue', alpha=0.075)
     plt.scatter(X, rfActiveF1[sampleSize][10], s=8, c='Red', alpha=0.075)
-    #plt.scatter(X, svmSysF1[sampleSize][:50], s=8, c='Green', alpha=0.075)
+    #plt.scatter(X, svmSysF1[sampleSize], s=8, c='Green', alpha=0.075)
 
     rand_means.append(np.mean(rfRandomF1[sampleSize]))
     act_means.append(np.mean(rfActiveF1[sampleSize][10]))
-    sys_means.append(np.mean(svmSysF1[sampleSize][10]))
+    #sys_means.append(np.mean(svmSysF1[sampleSize][10]))
 
 plt.plot(range(50, 211, 10), rand_means,linewidth=3.0,alpha=0.5)
 plt.plot(range(50, 211, 10), act_means,linewidth=3.0, c='Red',alpha=0.5)
+#plt.plot(range(50, 211, 10), sys_means,linewidth=3.0, c='Green',alpha=0.5)
+
+print(np.mean([act_means[i] - rand_means[i] for i in range(len(rand_means))]))
 
 red_patch = mpatches.Patch(color='red', label='Active')
 blu_patch = mpatches.Patch(color='blue', label='Random')
 plt.legend(handles=[red_patch,blu_patch],loc=(0.75, 0.05))
+plt.title('RF F1 Scores: Active vs Random')
 #plt.plot(range(50, 301, 10), sys_means,linewidth=3.0, c='Green',alpha=0.5)
 plt.show()
